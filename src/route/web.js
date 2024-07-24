bd7d80d9842d985f0e0f7700b88eb5efedb3e2bf
Hoang Sang <st319913@gmail.com>
2024-07-24 10:02:28 +0700

    feat api detail doctor

--- src/route/web.js
+++ src/route/web.js
@@ -39,11 +39,15 @@ let initWebRoutes = (app) => {
   router.post("/api/create-new-user", userController.handleCreateNewUser);
   router.put("/api/edit-user", userController.handleEditUser);
   router.delete("/api/delete-user", userController.handleDeleteUser);
-
   router.get("/api/allcode", userController.getAllCode);
 
   //doctor
-  router.get("/api/top-doctor-home", doctorController.getTopDoctorHome)
+  router.get("/api/top-doctor-home", doctorController.getTopDoctorHome);
+  router.get("/api/get-all-doctors", doctorController.getAllDoctors)
+  router.post("/api/save-infor-doctor", doctorController.postInforDoctor);
+
+  router.get("/api/get-detail-doctor", doctorController.getDetailDoctorById)
+
 
   return app.use("/", router);
 };
